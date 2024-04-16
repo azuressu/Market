2f7a46ae58c3fc0c61c5a001bddc8c78eb1e227c
azuressu <martina0130@naver.com>
2024-04-16 19:20:10 +0900

    :tada: git init

---
+++ src/main/java/com/spartamarket/SpartamarketApplication.java
@@ -0,0 +1,13 @@
+package com.spartamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartamarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartamarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spartamarket/SpartamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
