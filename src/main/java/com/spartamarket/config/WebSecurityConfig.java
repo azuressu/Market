9981628f9ec09c1bcbd29b61e654b9cbebd859ee
azuressu <martina0130@naver.com>
2024-04-17 16:10:56 +0900

    :sparkles: create security config, jwtutil

---
+++ src/main/java/com/spartamarket/config/WebSecurityConfig.java
@@ -0,0 +1,96 @@
+package com.spartamarket.config;
+
+import com.spartamarket.filter.JwtFilter;
+import com.spartamarket.filter.LoginFilter;
+import com.spartamarket.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration     // configuration 클래스 등록
+@EnableWebSecurity // web security 활성화
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    /**
+     * SecurityConfig
+     * 스프링 세큐리티의 인가 및 설정을 담당하는 클래스
+     * SecurityConfig 구현은 Security 버전별로 상이하므로 주의하기
+     */
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtUtil jwtUtil;
+
+    // AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    /**
+     * 비밀번호 인코딩 메소드 빈 등록
+     * BCrypt 해싱 알고리즘을 사용해 비밀번호를 안전하게 해싱
+     * 이렇게 함으로써 사용자의 비밀번호가 안전하게 저장되고, 보안을 향상시킴
+     */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 세큐리티 필터 체인 빈 등록
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // CSRF Disable
+        httpSecurity.csrf((auth) -> auth.disable());
+
+        // Form 로그인 방식 Disable
+        httpSecurity.formLogin((auth) -> auth.disable());
+
+        // Http Basic 인증 방식 Disable
+        httpSecurity.httpBasic((auth) -> auth.disable());
+
+        // 경로별로 인가 작업
+        httpSecurity
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .anyRequest().authenticated());
+
+        httpSecurity
+                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+
+        httpSecurity
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        /**
+         * 세션 설정
+         * JWT를 통한 인증/인가를 위해 세션을 STATELESS 상태로 설정하는 것이 중요
+         *
+            * 확장성 - 서버의 부하를 줄이고 확작성 향상
+            * 분산 클라이언트 환경에서의 유용성 - 분산 시스템에서 세션 동기화 문제 해결
+            * 클라이언트 측 저장소 활용 - 클라이언트 측에서도 상태를 유지할 수 있어 효율적
+            * 보안 - 세션 데이터를 서버에 저장하지 않고도 보안 유지 가능
+         */
+        httpSecurity
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/com/spartamarket/jwt/JwtUtil.java
@@ -0,0 +1,128 @@
+package com.spartamarket.jwt;
+
+import com.spartamarket.entity.UserRoleEnum;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    public static final String AUTHORIZATION_KEY = "auth";
+
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    private final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;     // Base64 인코드 한 Secretkey
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    // 로그 설정
+    public static final Logger logger = LoggerFactory.getLogger("JWT 로그");
+
+    // 토큰 생성
+    public String createToken(String username, UserRoleEnum role) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(username)           // 사용자 식별자 값(ID)
+                        .claim(AUTHORIZATION_KEY, role) // 사용자 권한
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME))  // 토큰의 만료 시간
+                        .setIssuedAt(date) // 발급일
+                        .signWith(key, signatureAlgorithm)  // 암호화 알고리즘
+                        .compact();
+    }
+
+    // Header에서 JWT 가져오기
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        // 토큰 출력
+        logger.info(bearerToken);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7); // "Bearer " 까지 자름
+        }
+
+        return null;
+    }
+
+    // Request에서 JWT 가져오기
+    public String getTokenFromRequest(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        logger.info(Arrays.toString(cookies));
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(AUTHORIZATION_HEADER)) {
+                    try {
+                        // Encode 되어 넘어간 Value를 다시 Decode
+                        return URLDecoder.decode(cookie.getValue(), "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        return null;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    // JWT 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            logger.error("Invalid JWT signature, 유효하지 않은 JWT 서명");
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token, 만료된 JWT");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token, 지원되지 않는 JWT");
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims is empty, 잘못된 JWT");
+        }
+        return false;
+    }
+
+    // JWT 에서 사용자 정보 가져오기
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parser().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    // JWT 자르기
+    public String subStringToken(String tokenValue) {
+        if(StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        logger.error("Not Found Token, 토큰이 없습니다");
+        throw new NullPointerException("Not Found Token, 토큰 없음");
+    }
+
+
+}
