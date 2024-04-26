1a51d03d09ad345189d139fddf045b29e06b733a
azuressu <martina0130@naver.com>
2024-04-26 22:19:25 +0900

    :memo: update elasticsearch config

--- src/main/java/com/spartamarket/config/ElasticSearchConfig.java
+++ src/main/java/com/spartamarket/config/ElasticSearchConfig.java
@@ -1,15 +1,16 @@
 package com.spartamarket.config;
 
+import com.spartamarket.entity.Productsearch;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories
+@EnableElasticsearchRepositories(basePackageClasses = Productsearch.class)
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder().connectedToLocalhost().build();
+        return ClientConfiguration.builder().connectedTo("localhost:9200").withBasicAuth("elastic", "091016").build();
     }
 }
