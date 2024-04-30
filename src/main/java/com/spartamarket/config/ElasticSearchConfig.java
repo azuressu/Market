837fe3f12debbeb7d72d79f8033fceabff9dca8c
azuressu <martina0130@naver.com>
2024-04-30 21:19:51 +0900

    :memo: update elasticsearch config

--- src/main/java/com/spartamarket/config/ElasticSearchConfig.java
+++ src/main/java/com/spartamarket/config/ElasticSearchConfig.java
@@ -1,16 +1,74 @@
 package com.spartamarket.config;
 
 import com.spartamarket.entity.Productsearch;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+import javax.net.ssl.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
 @Configuration
 @EnableElasticsearchRepositories(basePackageClasses = Productsearch.class)
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.data.elasticsearch.cluster-name}")
+    private String username;
+
+    @Value("${spring.data.elasticsearch.cluster-password}")
+    private String password;
+
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder().connectedTo("localhost:9200").withBasicAuth("elastic", "091016").build();
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .usingSsl(disableSslVerification(), allHostsValid())
+                .withBasicAuth(username, password)
+                .build();
     }
+
+    public static SSLContext disableSslVerification() {
+        /**
+         * SSL 인증서를 체크하지 않도록 설정
+         */
+        try {
+            // TrustManager 생성(인증서 체크 전부 안함)
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            // trust manager 설치
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            return sc;
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static HostnameVerifier allHostsValid() {
+        // HostnameVerifier 생성(호스트 네임 확인 X)
+        HostnameVerifier allHostsValid = (hostname, session) -> true;
+
+        // HostnameVerifier 설치
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        return allHostsValid;
+    }
+
 }
+
