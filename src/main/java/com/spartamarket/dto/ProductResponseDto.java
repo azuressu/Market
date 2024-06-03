89cace77945d34ce33b62c2f0a7c868c48c7dd5a
azuressu <martina0130@naver.com>
2024-06-03 21:54:15 +0900

    :memo: update product responsedto (add createdAt)

--- src/main/java/com/spartamarket/dto/ProductResponseDto.java
+++ src/main/java/com/spartamarket/dto/ProductResponseDto.java
@@ -1,9 +1,12 @@
 package com.spartamarket.dto;
 
 import com.spartamarket.entity.Product;
+import com.spartamarket.entity.ProductDocument;
 import jakarta.transaction.Status;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 
 @Getter
 @NoArgsConstructor
@@ -13,13 +16,22 @@ public class ProductResponseDto extends StatusResponseDto {
     private String content;
     private Integer price;
     private String username;
+    private String createdAt;
 
     public ProductResponseDto(Product product) {
         this.title = product.getTitle();
         this.content = product.getContent();
         this.price = product.getPrice();
         this.username = product.getUser().getUsername();
+        this.createdAt = String.valueOf(product.getCreatedAt());
     }
 
+    public ProductResponseDto(ProductDocument productDocument) {
+        this.title = productDocument.getTitle();
+        this.content = productDocument.getContent();
+        this.price = productDocument.getPrice();
+        this.username = productDocument.getUsername();
+        this.createdAt = String.valueOf(productDocument.getCreatedAt());
+    }
 
 }
