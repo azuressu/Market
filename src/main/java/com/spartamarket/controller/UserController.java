0a6a91a8cd1b14974b3a85d61da2800ab8be2934
azuressu <martina0130@naver.com>
2024-06-11 23:38:13 +0900

    :memo: update usercontroller (change return datatype for redirect)

--- src/main/java/com/spartamarket/controller/UserController.java
+++ src/main/java/com/spartamarket/controller/UserController.java
@@ -1,17 +1,11 @@
 package com.spartamarket.controller;
 
-import com.spartamarket.service.UserService;
 import com.spartamarket.dto.JoinRequestDto;
-import com.spartamarket.dto.StatusResponseDto;
-
+import com.spartamarket.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,13 +13,14 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/join")
-    public ResponseEntity<StatusResponseDto> joinSpartaMarket(@Valid @RequestBody JoinRequestDto joinRequestDto, BindingResult bindingResult) {
+    @PostMapping("/api/join")
+    public String joinSpartaMarket(@Valid JoinRequestDto joinRequestDto) {
         try {
             userService.joinSpartaMarket(joinRequestDto);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(new StatusResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
+            return e.getMessage();
         }
-        return ResponseEntity.status(201).body(new StatusResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
+        return "redirect:/api/login";
+        // return ResponseEntity.status(201).body(new StatusResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
     }
 }
