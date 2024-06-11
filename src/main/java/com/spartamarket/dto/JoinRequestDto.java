317269c225af80c049415f9877ccb3d159ecff31
azuressu <martina0130@naver.com>
2024-06-11 23:39:09 +0900

    :memo: update join requestdto (add patterns)

--- src/main/java/com/spartamarket/dto/JoinRequestDto.java
+++ src/main/java/com/spartamarket/dto/JoinRequestDto.java
@@ -16,6 +16,9 @@ public class JoinRequestDto {
     @Pattern(regexp = "^[a-zA-Z0-9#?!@$%^&*-]{8,15}$", message = "최소 8자 이상 15자 이하이며, 알파벳 대소문자(A-Z, a-z) 및 숫자(0-9) 및 특수문자로 구성 필수")
     private String password;
 
+    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@(.+)$", message = "이메일 형식에 맞지 않습니다")
+    private String email;
+
     private String nickname;
 
     private Boolean admin = false;   // 기본을 admin이 아닌 일반 회원으로 설정
