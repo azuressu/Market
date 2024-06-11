60d9f965767a0f6ee51757e873de3e71583b0ae9
azuressu <martina0130@naver.com>
2024-06-11 23:39:35 +0900

    :memo: update login requestdto (add jsonformat)

--- src/main/java/com/spartamarket/dto/LoginRequestDto.java
+++ src/main/java/com/spartamarket/dto/LoginRequestDto.java
@@ -1,12 +1,17 @@
 package com.spartamarket.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class LoginRequestDto {
 
+    @JsonFormat
     public String username;
+    @JsonFormat
     public String password;
 }
