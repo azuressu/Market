21f85c5f9f29d880d463aff0ee260bc98b8ddb85
azuressu <martina0130@naver.com>
2024-06-11 23:40:00 +0900

    :memo: update view controller

--- src/main/java/com/spartamarket/controller/ViewController.java
+++ src/main/java/com/spartamarket/controller/ViewController.java
@@ -1,5 +1,9 @@
 package com.spartamarket.controller;
 
+import com.spartamarket.jwt.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,20 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
-@RequestMapping("/api")
 public class ViewController {
 
-    @GetMapping("/main")
+    @GetMapping("/")
     public String mainPage() {
         return "index";
     }
 
-    @GetMapping("/join")
+    @GetMapping("/api/product")
+    public String productPage() {
+        return "products";
+    }
+
+    @GetMapping("/api/join")
     public String joinPage() {
         return "join";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/api/login")
     public String loginPage() {
         return "login";
     }
