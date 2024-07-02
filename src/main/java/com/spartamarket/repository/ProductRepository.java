1198748dd68f81e24b792cfab9caef4eee9bc9ee
azuressu <martina0130@naver.com>
2024-07-02 23:53:40 +0900

    :memo: update product repo (update method)

--- src/main/java/com/spartamarket/repository/ProductRepository.java
+++ src/main/java/com/spartamarket/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.spartamarket.repository;
 
 import com.spartamarket.entity.Product;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,6 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findByTitle(String title);
+    Slice<Product> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
     Page<Product> findAll(Pageable pageable);
 }
