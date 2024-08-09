ecd16cdfd11815e89b04c5bf987c6ae5f0fd78b9
azuressu <martina0130@naver.com>
2024-08-09 23:48:00 +0900

    :sparkles: create timestamped

---
+++ src/main/java/com/spartamarket/entity/ElasticsearchTimestamped.java
@@ -0,0 +1,27 @@
+package com.spartamarket.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Document(indexName = "elastictimestamped")
+public abstract class ElasticsearchTimestamped {
+
+    @CreatedDate
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime updatedAt;
+}
